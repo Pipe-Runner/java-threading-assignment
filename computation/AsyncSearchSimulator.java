6b3627b93b877e30b3f5045fb5ad164cb740cd3b
Pipe-Runner <piperunner.in@gmail.com>
2023-01-27 12:34:20 +0100

    Removed complied files

---
+++ computation/AsyncSearchSimulator.java
@@ -0,0 +1,48 @@
+package computation;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+import utils.ResponseGenerator;
+
+/**
+
+ */
+public class AsyncSearchSimulator implements Runnable {
+
+  protected Socket clientSocket = null;
+  protected String serverText = null;
+
+  public AsyncSearchSimulator(Socket clientSocket, String serverText) {
+    this.clientSocket = clientSocket;
+    this.serverText = serverText;
+  }
+
+  public void run() {
+    try {
+      InputStream input = clientSocket.getInputStream();
+      OutputStream output = clientSocket.getOutputStream();
+
+      long time1 = System.currentTimeMillis();
+      System.out.println("Request processing started at: " + time1);
+      Thread.sleep(10 * 1000);
+      long time2 = System.currentTimeMillis();
+      System.out.println("Request processing ended at: " + time2);
+
+      // Generating response
+      byte[] responseDocument = ResponseGenerator.generatorResponseHTML(time1, time2).getBytes("UTF-8");
+      byte[] responseHeader = ResponseGenerator.generatorResponseHeader(responseDocument.length).getBytes("UTF-8");
+
+      output.write(responseHeader);
+      output.write(responseDocument);
+      output.close();
+      input.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
---
+++ servers/SingleThreadedServer.java
@@ -0,0 +1,70 @@
+package servers;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import computation.SearchSimulator;
+
+import java.io.IOException;
+
+public class SingleThreadedServer implements Runnable {
+
+    protected int serverPort = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
+
+    public SingleThreadedServer(int port) {
+        this.serverPort = port;
+    }
+
+    public void run() {
+        openServerSocket();
+
+        while (!isStopped()) {
+            Socket clientSocket = null;
+            
+            try {
+                // wait for a client to connect
+                System.out.println("Listening for connection.");
+                clientSocket = this.serverSocket.accept();
+            } catch (IOException e) {
+                if (isStopped()) {
+                    System.out.println("Server Stopped.");
+                    return;
+                }
+                throw new RuntimeException(
+                        "Error accepting client connection", e);
+            }
+            
+            try {
+                SearchSimulator.processClientRequest(clientSocket);
+            } catch (Exception e) {
+                // log exception and go on to next request.
+            }
+        }
+
+        System.out.println("Server Stopped.");
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open port 8080", e);
+        }
+    }
+}
